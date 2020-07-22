98dbffd9916e3ccfc7c614308a6a37d03fc5418e
lhmoreno <lhmorenoti@hotmail.com>
2020-07-22 20:34:12 -0300

    add fonts

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import Climate from './src/Climate';
+import React from 'react'
+import Climate from './src/Climate'
+import { useFonts, JosefinSans_700Bold, JosefinSans_300Light } from '@expo-google-fonts/josefin-sans'
+import { AppLoading } from 'expo'
 
 export default function App() {
-  
+  const [fontsLoaded] = useFonts({
+    JosefinSans_700Bold,
+    JosefinSans_300Light
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <Climate />
-  );
+  )
 }
